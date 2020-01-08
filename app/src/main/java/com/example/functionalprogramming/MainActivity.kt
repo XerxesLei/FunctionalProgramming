9846eb38f683e6853f08582aa7877a66f15b0f44
dpro <398170146@qq.com>
2020-01-08 18:19:13 +0800

    android 动画源码分析

--- app/src/main/java/com/example/functionalprogramming/MainActivity.kt
+++ app/src/main/java/com/example/functionalprogramming/MainActivity.kt
@@ -2,12 +2,30 @@ package com.example.functionalprogramming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var a = 1f
+        text.setOnClickListener {
+            println("text click")
+            text.startAnimation(TranslateAnimation(-100f,100f, -100f, 100f).apply {
+                duration = 1000
+                repeatMode = Animation.REVERSE
+                fillAfter = true
+            })
+
+            text.animate().setDuration(1000)
+                .translationX(a++)
+                .translationY(a++)
+                .start()
+        }
 
         FunctionalProgramming.setFunctionalProgramming {
             println("FunctionalProgramming is run")
---
+++ app/src/main/java/com/example/functionalprogramming/java/java.java
@@ -0,0 +1,17 @@
+package com.example.functionalprogramming.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class java {
+
+    public static void main(String[] args) {
+
+        List li = new ArrayList<Integer>();
+        for (int i = 0; i < 100000; i++) {
+            li.add(i);
+        }
+
+        System.out.println(li);
+    }
+}
