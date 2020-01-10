fd86384f690246a27955c8cac2f49087406bb9d0
dpro <398170146@qq.com>
2020-01-10 19:11:44 +0800

    异想天开

--- app/src/main/java/com/example/functionalprogramming/MainActivity.kt
+++ app/src/main/java/com/example/functionalprogramming/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.example.functionalprogramming
 
-import android.app.ActivityManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.animation.Animation
 import android.view.animation.TranslateAnimation
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.example.functionalprogramming.java.TestClass
 import kotlinx.android.synthetic.main.activity_main.*
-import java.nio.file.Paths
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mViewPager2: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mViewPager2 = horViewPager
+
+        mViewPager2.apply {
+            adapter = Adapter()
+        }
         var a = 1f
         text.setOnClickListener {
             println("text click")
@@ -28,11 +36,25 @@ class MainActivity : AppCompatActivity() {
                 .translationY(a++)
                 .start()
         }
+
+        val testClass = TestClass()
 //
 //        FunctionalProgramming.setFunctionalProgramming {
 //            println("FunctionalProgramming is run")
 //        }
 //        FunctionalProgramming.start()
         println(" 1111111" +MainActivity::class.java.getResource("/"))
+    }
+
+    private inner class Adapter : FragmentStateAdapter(this){
+        override fun getItemCount(): Int {
+            return 3
+        }
+
+        override fun createFragment(position: Int): Fragment {
+
+            return ContentFragment.newInstance("1", "2")
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/functionalprogramming/java/TestClass.java
@@ -0,0 +1,16 @@
+package com.example.functionalprogramming.java;
+
+import android.graphics.Point;
+
+public class TestClass {
+
+    private float x = 0f;
+
+    public Float getX(){
+        return x;
+    }
+
+    public void setX(Float x) {
+        this.x = x;
+    }
+}
